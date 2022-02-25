a3ca44a901d6d146f04c269f7a93dd7aee8d6b9d
Thanathat <74446225+Thanathat159@users.noreply.github.com>
2022-02-25 20:09:23 +0700

    add pallax

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './hero.css'
 import ImgP from '../../images/podcast.svg'
 
 const Hero = () => {
+
+    const [offsetY, setOffsetY] = useState(0);
+
+    const handleScroll = () =>setOffsetY(window.pageYOffset);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
+
     return (
         <>
-            <section id="hero" >
-                <div  className="flex pt-72 flex-col text-white ml-24 " >
+            <section id="hero"   >
+                <div style={{ transform: `translateY(${offsetY * 0.5}px )`}}  className="flex pt-72 flex-col text-white ml-24 " >
                     <h1 className="text-7xl font-bold">Night Podcast</h1>
                     <p className="text-4xl leading-relaxed" >Listen to our Podcast a night after <br /> <span>a tiring day</span></p>
                     <img className="w-40 mt-4 cursor-pointer" src={ImgP} alt="" />
